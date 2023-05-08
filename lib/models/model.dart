6de3aeca6fd6963c4933675aac4c3c32f62a6e9d
TonnyKamau <tonnykamau6@gmail.com>
2023-05-08 21:49:11 +0300

    purging excess models

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -1,4 +1,2 @@
-export 'cappuccino_model.dart';
-export 'latte_model.dart';
-export 'espresso_model.dart';
 export 'special_model.dart';
+export 'coffee_model.dart';
--- lib/screens/coffee_screen.dart
+++ lib/screens/coffee_screen.dart
@@ -1,16 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../models/model.dart';
 
 class CoffeeScreen extends StatelessWidget {
   const CoffeeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Coffee'),
+    Coffee coffee = Get.arguments ?? Coffee.coffees[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(coffee.image),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            iconTheme: const IconThemeData(color: Colors.white),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () => Get.back(),
+            ),
+          ),
+          extendBodyBehindAppBar: true,
+          body: ListView(
+            children: [
+              _HeadLine(
+                coffee: coffee,
+              ),
+              _Body(coffee: coffee),
+            ],
+          ),
+        ),
       ),
-      body: Center(
-        child: Text('Coffee'),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({
+    Key? key,
+    required this.coffee,
+  }) : super(key: key);
+
+  final Coffee coffee;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40.0),
+          topRight: Radius.circular(40.0),
+        ),
+      ),
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
+
+class _HeadLine extends StatelessWidget {
+  const _HeadLine({
+    Key? key,
+    required this.coffee,
+  }) : super(key: key);
+  final Coffee coffee;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
       ),
     );
   }
