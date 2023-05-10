6581bdfa72a41b7212cb9961c6bda4b8e47e55b9
TonnyKamau <tonnykamau6gmail.com>
2023-05-10 19:01:42 +0300

    header section of the coffee screen

--- lib/screens/coffee_screen.dart
+++ lib/screens/coffee_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
 import '../models/model.dart';
@@ -16,35 +17,36 @@ class CoffeeScreen extends StatelessWidget {
           elevation: 0.0,
           leading: Container(
             margin: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               color: Colors.white,
-              borderRadius: BorderRadius.circular(50.0),
+              shape: BoxShape.circle,
             ),
             child: Center(
               child: IconButton(
                 onPressed: () {
                   Get.back();
                 },
-                icon: const Icon(Icons.arrow_back_ios),
+                icon: const FaIcon(FontAwesomeIcons.angleLeft),
                 color: Colors.black,
-                iconSize: 15,
+                iconSize: 20,
               ),
             ),
           ),
           actions: [
             Container(
               margin: const EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.circular(50.0),
+                shape: BoxShape.circle,
               ),
               child: Center(
                 child: IconButton(
                   onPressed: () {
                     print('Favorite');
                   },
-                  icon: const Icon(Icons.favorite_border),
+                  icon: const FaIcon(FontAwesomeIcons.heart),
                   color: Colors.black,
+                  iconSize: 20,
                 ),
               ),
             ),
@@ -70,51 +72,94 @@ class _NewsHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ImageContainer(
+    return Container(
       height: MediaQuery.of(context).size.height * 0.45,
       width: double.infinity,
-      rating: '',
-      padding: const EdgeInsets.all(20.0),
-      imageUrl: coffee.image,
+      padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical:10.0),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.black.withOpacity(0.7),
+            Colors.black.withOpacity(0.4),
+            Colors.black.withOpacity(0.1),
+            Colors.transparent,
+          ],
+        ),
+        image: DecorationImage(
+          image: NetworkImage(coffee.image),
+          fit: BoxFit.cover,
+        ),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CustomTag(backgroundColor: Colors.grey.withAlpha(150), children: [
-            Text('News of the Day',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium!
-                    .copyWith(color: Colors.white)),
-          ]),
-          const SizedBox(height: 10.0),
-          Text(coffee.type,
-              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                  fontWeight: FontWeight.bold,
-                  height: 1.25,
-                  color: Colors.white)),
-          TextButton(
-            onPressed: () {
-              print('Read More');
-            },
-            style: TextButton.styleFrom(
-              padding: EdgeInsets.zero,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(coffee.type,
+                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      height: 1.25,
+                      color: Colors.white)),
+              _RatingTag(coffee: coffee),
+            ],
+          ),
+          Text(
+            coffee.description,
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium!
+                .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _RatingTag extends StatelessWidget {
+  const _RatingTag({
+    Key? key,
+    required this.coffee,
+  }) : super(key: key);
+
+  final Coffee coffee;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20,
+      width: 50,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(161, 72, 39, 0.7),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.star,
+            size: 12,
+            color: Colors.white,
+          ),
+          const SizedBox(
+            width: 2,
+          ),
+          Text(
+            coffee.rating,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 12,
             ),
-            child: Row(
-              children: [
-                Text(
-                  'Read More',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyLarge!
-                      .copyWith(color: Colors.white),
-                ),
-                const SizedBox(width: 10.0),
-                const Icon(Icons.arrow_forward_ios,
-                    color: Colors.white, size: 15.0),
-              ],
-            ),
-          )
+          ),
         ],
       ),
     );
